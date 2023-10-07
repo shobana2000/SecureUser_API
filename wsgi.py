e8569a788e45e3fee91113c1fd2eaf61b075f7e3
Shobana <48212587+shobana2000@users.noreply.github.com>
2023-10-07 11:04:18 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-workers = 2  # Number of worker processes (you can adjust this)
-bind = "0.0.0.0:8000"  # Host and port where your application will run
-loglevel = "info"  # Log level (you can set it to "debug" for debugging)
-
-
